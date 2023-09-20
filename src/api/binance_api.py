2ce8e668cc9a57a79d07afdd2ac00bbc0260a21c
lucidaniel <daniel.hajduk@protonmail.com>
2023-09-20 15:28:25 +0200

    api.py bug fix

--- src/api/binance_api.py
+++ src/api/binance_api.py
@@ -1,32 +1,15 @@
 import aiohttp
 import asyncio
-import yaml
+import os
 import pandas as pd
 import logging
-import os
-
-# Use environment variables
-from utils.helpers import setup_logging, get_env_variable
 
 # Initialize logging
-setup_logging()
+logging.basicConfig(level=logging.INFO)
 
-# Get the absolute path to the directory where your script is located
-script_location = get_env_variable('SCRIPT_LOCATION')
-
-# Build the absolute path to your settings.yaml file
-config_file_path = os.path.join(script_location, 'config', 'settings.yaml')
-
-try:
-    with open(config_file_path, 'r') as stream:
-        config = yaml.safe_load(stream)
-        logging.info("Loaded settings from config file.")
-except Exception as e:
-        logging.error(f"Failed to load config file: {e}")
-        raise
-
-api_key = config['binance']['api_key']
-api_secret = config['binance']['api_secret']  # Make sure to use this securely
+# Use environment variables for API keys
+api_key = os.environ.get('BINANCE_API_KEY')
+api_secret = os.environ.get('BINANCE_API_SECRET')  # Make sure to use this securely
 
 async def fetch_historical_data(symbol, timeframe, since, limit):
     try:
@@ -34,18 +17,31 @@ async def fetch_historical_data(symbol, timeframe, since, limit):
         headers = {'X-MBX-APIKEY': api_key}
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=headers) as response:
+                if response.status != 200:
+                    raise aiohttp.ClientError(f"Failed to fetch data: {response.status}")
                 data = await response.json()
-            logging.info(f"Fetched historical data for {symbol}.")
-            return data
-    except Exception as e:
-            logging.error(f"Failed to fetch historical data for {symbol}: {e}")
-            return None
+        logging.info(f"Fetched historical data for {symbol}.")
+        return data
+    except aiohttp.ClientError as e:
+        logging.error(f"Failed to fetch historical data for {symbol}: {e}")
+        return None
 
 async def save_historical_data_to_csv(symbol, data):
     try:
         df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        historical_data_path = os.path.join(script_location, '..', '..', 'data', 'historical_data', f"{symbol}_historical_data.csv")
+        historical_data_path = os.path.join('data', 'historical_data', f"{symbol}_historical_data.csv")
+        df.to_csv(historical_data_path, index=False)
+        logging.info(f"Saved historical data to CSV for {symbol}.")
+    except Exception as e:
+        logging.error(f"Failed to save historical data to CSV: {e}")
+        return None
+
+async def save_historical_data_to_csv(symbol, data):
+    try:
+        df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+        historical_data_path = os.path.join('data', 'historical_data', f"{symbol}_historical_data.csv")
         df.to_csv(historical_data_path, index=False)
         logging.info(f"Saved historical data to CSV for {symbol}.")
     except Exception as e:
