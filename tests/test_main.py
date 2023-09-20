da5cebb6d27ff04f10968406805ef38202ec0a94
lucidaniel <daniel.hajduk@protonmail.com>
2023-09-20 17:55:43 +0200

    fixing with AI

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,10 +4,13 @@ from src.main import fetch_current_price, analyze_symbol
 class TestMain(asynctest.TestCase):
     async def test_analyze_symbol(self):
         symbols = ['BTC/USDT', 'ETH/USDT', 'SOL/USDT']
+        # TODO: Add assertions and logic to test analyze_symbol function
     
     async def test_invalid_symbol(self):
-        # Add a test case for invalid symbols
-        pass
+        # TODO: Add a test case for invalid symbols
+    
+    async def test_fetch_current_price(self):
+        symbols = ['BTC/USDT', 'ETH/USDT', 'SOL/USDT']
         # Mocking the API call to return a float (e.g., 50000.0)
         with asynctest.patch('src.main.binance.fetch_ticker', return_value={'last': 50000.0}):
             for symbol in symbols:
