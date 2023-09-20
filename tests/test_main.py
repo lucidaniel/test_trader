1e913839b6ed4c5bb89c198c60fae0c5a473e61a
lucidaniel <daniel.hajduk@protonmail.com>
2023-09-20 18:56:00 +0200

    test fix AI

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,13 @@ from src.main import fetch_current_price, analyze_symbol
 class TestMain(asynctest.TestCase):
     async def test_analyze_symbol(self):
         symbols = ['BTC/USDT', 'ETH/USDT', 'SOL/USDT']
-        # TODO: Add assertions and logic to test analyze_symbol function
+        result = await analyze_symbol('BTC/USDT')
+        assert result is not None  # Replace with actual assertions
     
     async def test_invalid_symbol(self):
         # TODO: Add a test case for invalid symbols
     
-         def test_fetch_current_price():
+         def test_fetch_current_price(self):
            assert True  # Placeholder
            assert True  # Placeholder
 
