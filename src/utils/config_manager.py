33c84b16046bb14505b5eeb7823201a3b42901f7
lucidaniel <daniel.hajduk@protonmail.com>
2023-09-20 21:51:41 +0200

    komit

--- src/utils/config_manager.py
+++ src/utils/config_manager.py
@@ -1,19 +1,20 @@
 import yaml
 import logging
 from src.initialize import initialize_app
+from typing import Any, Dict
 
 initialize_app()
 
 class ConfigManager:
-    def __init__(self, config_path):
+    def __init__(self, config_path: str) -> None:
         self.config = self.load_config(config_path)
         self.validate_config()
 
-    def load_config(self, config_path):
+    def load_config(self, config_path: str) -> Dict[str, Any]:
         try:
             with open(config_path, 'r') as stream:
                 config = yaml.safe_load(stream)
-                logging.info("Loaded configuration from file.")
+                logging.info("Successfully loaded configuration from file.")
                 return config
         except FileNotFoundError:
             logging.error(f"{config_path} not found.")
@@ -22,12 +23,13 @@ class ConfigManager:
             logging.error(f"Failed to load config file: {e}")
             raise
 
-    def validate_config(self):
+    def validate_config(self) -> None:
+        """Validates the loaded configuration to ensure it contains the necessary sections."""
         if 'binance' not in self.config:
             logging.error("Missing 'binance' section in config.")
             raise ValueError("Invalid configuration.")
-        # Example validation for trading pairs
         if 'trading_pairs' not in self.config:
             logging.error("Missing 'trading_pairs' section in config.")
             raise ValueError("Invalid configuration.")
+        logging.info("Configuration validated successfully.")
         # Add further validation logic based on your specific needs
--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,10 +1,11 @@
 import logging
 import os
+from typing import Type, Any
 from src.initialize import initialize_app
 
 initialize_app()
 
-def setup_logging(log_level="INFO", log_file="logs/trading_bot.log"):
+def setup_logging(log_level: str = "INFO", log_file: str = "logs/trading_bot.log") -> None:
     """
     Sets up logging for the application.
     
@@ -17,20 +18,20 @@ def setup_logging(log_level="INFO", log_file="logs/trading_bot.log"):
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[
             logging.StreamHandler(),
-            logging.FileHandler(log_file)  # Log to a file
+            logging.FileHandler(log_file)
         ]
     )
 
-def get_env_variable(var_name, expected_type=str):
+def get_env_variable(var_name: str, expected_type: Type[Any] = str) -> Any:
     try:
         value = os.environ[var_name]
         validate_env_variable(var_name, value, expected_type)
         return value
     except KeyError:
         logging.error(f"Environment variable {var_name} not set.")
-        exit(1)
+        raise EnvironmentError(f"Environment variable {var_name} not set.")
 
-def validate_env_variable(var_name, value, expected_type):
+def validate_env_variable(var_name: str, value: Any, expected_type: Type[Any]) -> None:
     if not isinstance(value, expected_type):
         logging.error(f"Environment variable {var_name} is not of expected type {expected_type}.")
-        exit(1)
+        raise TypeError(f"Environment variable {var_name} is not of expected type {expected_type}.")
