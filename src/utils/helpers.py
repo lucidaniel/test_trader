015c2889436f5c2f421d2f8e714e8ed7d6601aba
lucidaniel <daniel.hajduk@protonmail.com>
2023-09-20 18:02:24 +0200

    fixing with AI

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -18,15 +18,16 @@ def setup_logging(log_level="INFO", log_file="logs/trading_bot.log"):
         ]
     )
 
-def get_env_variable(var_name):
+def get_env_variable(var_name, expected_type=str):
     try:
         value = os.environ[var_name]
-        validate_env_variable(var_name, value)  # New function to validate the value
+        validate_env_variable(var_name, value, expected_type)
         return value
     except KeyError:
         logging.error(f"Environment variable {var_name} not set.")
         exit(1)
 
-def validate_env_variable(var_name, value):
-    # Add validation logic here
-    pass
+def validate_env_variable(var_name, value, expected_type):
+    if not isinstance(value, expected_type):
+        logging.error(f"Environment variable {var_name} is not of expected type {expected_type}.")
+        exit(1)
