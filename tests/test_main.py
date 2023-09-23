0baa1a757a93f5aadae7f609087fa8d63ea71899
lucidaniel <daniel.hajduk@protonmail.com>
2023-09-23 19:26:23 +0200

    add tesrt_fetch_current_price

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,12 +7,13 @@ class TestMain(asynctest.TestCase):
         result = await analyze_symbol('BTC/USDT')
         assert result is not None  # Replace with actual assertions
     
-    async def test_invalid_symbol(self):
-        # TODO: Add a test case for invalid symbols
+    #async def test_invalid_symbol(self):
+        #TODO: Add a test case for invalid symbols
     
-        async def test_fetch_current_price(self):
-            assert True  # Placeholder
-            assert True  # Placeholder
-
+    async def test_fetch_current_price(self):
+        price = await fetch_current_price('BTC/USDT', 'ETH/USDT', 'SOL/USDT')
+        assert isinstance(price, float), "Price should be a float value"
+        assert price > 0, "Price should be greater than 0"
+    
 if __name__ == '__main__':
     asynctest.main()
